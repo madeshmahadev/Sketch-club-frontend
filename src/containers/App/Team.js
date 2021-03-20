20f628c3a3dfea336e852dab5c3ccc25be06fc48
SinghGobind <58527382+SinghGobind@users.noreply.github.com>
2021-03-20 18:03:15 +0530

    Update Team.js

--- src/containers/App/Team.js
+++ src/containers/App/Team.js
@@ -214,8 +214,12 @@ function Team() {
                   <div className="content">
                     <a className="header" href="https://www.instagram.com/thegobindsingh/" target="_blank">Gobind Singh</a>
                     <div className="meta">
-                      <span className="date">Student Coordinator</span>
-	  		<span className="date">Media Co-Manager</span>
+                      <span className="date">Student Coordinator,</span>
+                    </div>
+	  		<div className="meta">
+	  		<span className="date">Media Co-Manager,</span>
+                    </div>
+	  		<div className="meta">
 	  		<span className="date">R&D Sub-Manager</span>
                     </div>
                   </div>
