3096c2eeac753d7400f5b11adc01bcb8078c4920
GOBIND SINGH <android.gobind@gmail.com>
2021-07-15 22:29:39 +0530

    Update Upcoming.js

--- src/containers/App/components/Upcoming.js
+++ src/containers/App/components/Upcoming.js
@@ -5,7 +5,7 @@ function Upcoming() {
   return (
     <div className="ui vertical stripe segment">
       <h1 className="ui center aligned horizontal  header ">
-        Our Events<a class="ui teal empty circular label"></a><a class="ui blue empty circular label"></a><a class="ui blue empty circular label"></a>
+        Our Events<a class="ui blue empty circular label"></a><a class="ui blue empty circular label"></a><a class="ui blue empty circular label"></a>
       </h1>
 
       <Articles />
