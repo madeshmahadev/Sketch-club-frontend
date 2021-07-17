2c5fb6be68fd5207fd4ed7ca2c340ac096e79e50
GOBIND SINGH <android.gobind@gmail.com>
2021-07-17 17:28:04 +0530

    Update index.js

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,6 +2,18 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Moment from "react-moment";
 
+function col ()
+{
+  if(article.category.Name == "Upcoming Event")
+    {
+      return(<div className="ui teal ribbon label">{article.category.Name}</div>);
+    }
+  else
+    {
+      return(<div className="ui yellow ribbon label">{article.category.Name}</div>);
+    }
+}
+
 const Card = ({ article }) => {
   const imageUrl =  article.Media.url;
   return (
@@ -12,16 +24,7 @@ const Card = ({ article }) => {
 
                 <a class="ui card" href={`/events/${article.id}`}>
                   <div class="image">
-                  
-                    if(article.category.Name == "Upcoming Event")
-                    {
-                      return(<div className="ui teal ribbon label">{article.category.Name}</div>);
-                    }
-                    else
-                    {
-                      return(<div className="ui yellow ribbon label">{article.category.Name}</div>);
-                    }
-                  
+                   {col()}
                       <img src={imageUrl} alt={article.Title} />
                   </div>
 
